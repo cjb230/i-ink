0465ec55822a6c370b905127845cc3890d3f488e
cjb230 <james.barton@blueyonder.co.uk>
2025-06-07 16:48:01 +0200

    Run in a loop

--- i_ink/display.py
+++ i_ink/display.py
@@ -2,9 +2,10 @@ from PIL import Image
 from gpiozero import Device
 
 # from . import epd7in5_V2
+PREVIOUS_IMAGE: Image = Image.new("RGB", (480, 750), "white")
 
 
-def update_screen(image: Image):
+def conditional_update_screen(image: Image):
     """
     epd = epd7in5_V2.EPD()
     epd.init()
--- i_ink/main.py
+++ i_ink/main.py
@@ -1,10 +1,14 @@
+import time
 from PIL import Image
 
-from .display import update_screen
-from .trains import get_next_wkd_trains, filter_trains_for_display
+from .display import conditional_update_screen
+from .trains import get_next_wkd_trains
 from .render import render_all
 from .transform import transform_weather, transform_trains
 from .weather import fetch_forecast
+
+
+SLEEP_DURATION_S = 240
 
 
 def collect_all_data():
@@ -17,8 +21,17 @@ def collect_all_data():
 
 
 def run_all():
-    data = collect_all_data()
-    transformed_weather = transform_weather(data["weather"])
-    transformed_trains = transform_trains(data["trains"])
-    result_image: Image = render_all(transformed_trains, transformed_weather)
-    update_screen(result_image)
+    n = 1
+    while True:
+        print(f"Starting run {n}")
+        data = collect_all_data()
+        transformed_weather = transform_weather(data["weather"])
+        transformed_trains = transform_trains(data["trains"])
+        train_timestamp = transformed_trains["update_str"]
+        del transformed_trains["update_str"]
+        result_image: Image = render_all(transformed_trains, transformed_weather, train_timestamp)
+        conditional_update_screen(result_image)
+        result_image.save("all.png")
+        n += 1
+        print(f"Sleeping for {SLEEP_DURATION_S} seconds at {time.localtime()} .\n")
+        time.sleep(SLEEP_DURATION_S)
