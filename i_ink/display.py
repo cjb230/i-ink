34996cae3da9842e2dab22956cba0fb9a42fa363
cjb230 <james.barton@blueyonder.co.uk>
2025-06-14 11:55:06 +0200

    more WIP

--- i_ink/display.py
+++ i_ink/display.py
@@ -1,17 +1,5 @@
 from PIL import Image
-from gpiozero import Device
+# from gpiozero import Device
 
-from . import epd7in5_V2
+# from . import epd7in5_V2
 
-PREVIOUS_IMAGE: Image = Image.new("RGB", (480, 750), "white")
-
-
-def conditional_update_screen(image: Image):
-    
-    epd = epd7in5_V2.EPD()
-    epd.init()
-    buf = epd.getbuffer(image)
-    epd.display(buf)
-    Device.pin_factory.close()
-    
-    return
