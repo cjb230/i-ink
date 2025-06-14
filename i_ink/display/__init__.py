884244e2e859fde3bb97a72b62405becdba9c197
cjb230 <james.barton@blueyonder.co.uk>
2025-06-14 17:20:43 +0200

    Print which display is being used.

--- i_ink/display/__init__.py
+++ i_ink/display/__init__.py
@@ -3,7 +3,9 @@ from .interface import DisplayInterface
 try:
     from .real import RealDisplay
     _DisplayClass = RealDisplay
+    print("Using real display class.")
 except ImportError:
+    print("Could not import real display class.\nImporting mock display class instead...")
     from .mock import MockDisplay
     _DisplayClass = MockDisplay
 
