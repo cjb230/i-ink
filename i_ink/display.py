d95c7fce394333f0d824cf10c7596249c8edd4dd
James Barton <cjamesb230@gmail.com>
2025-06-14 11:51:16 +0200

    remove pin factory close

--- i_ink/display.py
+++ i_ink/display.py
@@ -7,11 +7,8 @@ PREVIOUS_IMAGE: Image = Image.new("RGB", (480, 750), "white")
 
 
 def conditional_update_screen(image: Image):
-    
     epd = epd7in5_V2.EPD()
     epd.init()
     buf = epd.getbuffer(image)
     epd.display(buf)
-    Device.pin_factory.close()
-    
     return
