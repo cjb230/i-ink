8d2ffc44b0afe82b024a451bec7c746edd3afd99
James Barton <cjamesb230@gmail.com>
2025-06-14 17:59:57 +0200

    Changes for real hardware

--- i_ink/display/real.py
+++ i_ink/display/real.py
@@ -1,14 +1,14 @@
-from display.interface import DisplayInterface
+from .interface import DisplayInterface
 
 class RealDisplay(DisplayInterface):
     def __init__(self):
         # Import hardware libs only when instantiating RealDisplay
         try:
-            import epd7in5_V2
+            from .epd7in5_V2 import EPD
             import RPi.GPIO as GPIO
         except ImportError as e:
             raise ImportError(f"RealDisplay unavailable: {e}")
-        self.epd = epd7in5_V2.EPD()
+        self.epd = EPD()
         self.epd.init()
     def show(self, image):
         buf = self.epd.getbuffer(image)
