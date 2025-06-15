061fded352e42454ca34f909f34278e0719df16f
cjb230 <james.barton@blueyonder.co.uk>
2025-06-15 23:25:13 +0200

    bash tweaks

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+exec >> /home/cjb/i-ink.log 2>&1
+set -x
 cd /home/cjb/repos/i-ink
-git pull
+git -C /home/cjb/repos/i-ink pull --ff-only || true
 source /home/cjb/repos/i-ink/myenv/bin/activate
 export GPIOZERO_PIN_FACTORY=pigpio
 exec python run.py
